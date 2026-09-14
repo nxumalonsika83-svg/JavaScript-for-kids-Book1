3670a788167579f06035b4793f08e52f05a56835
Nsika Nxumalo <nxumalonsika83@gmail.com>
2026-09-14 14:14:19 +0200

    Update Page 94 & 96.js

--- Chap06/Page 94 & 96.js
+++ Chap06/Page 94 & 96.js
@@ -1,8 +1,11 @@
 var lemonChicken = false;
+Underfined 
 var beefWithBlackBean = true;
+Undefined 
 var sweetAndSourPork = true;
+Undefined 
 if (lemonChicken) {
- console.log("Great! I'm having lemon chicken!");
+ console.log("Great! I'm having lemon chicken!"); 
 } else if (beefWithBlackBean) {
  console.log("I'm having the beef.");
 } else if (sweetAndSourPork) {
@@ -10,7 +13,8 @@ if (lemonChicken) {
 } else {
  console.log("Well, I guess I'll have rice then.");
 }
-
+I'm having the beef.
+ 
 var lemonChicken = false;
 var beefWithBlackBean = false;
 var sweetAndSourPork = false;
@@ -21,3 +25,4 @@ if (lemonChicken) {
 } else if (sweetAndSourPork) {
  console.log("OK, I'll have the pork.");
 };
+Undefined
