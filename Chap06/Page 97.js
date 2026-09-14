aef9eed9c78e0cbe4ac99980c357c95d95e1108e
Nsika Nxumalo <nxumalonsika83@gmail.com>
2026-09-14 14:17:27 +0200

    Update Page 97.js

--- Chap06/Page 97.js
+++ Chap06/Page 97.js
@@ -4,3 +4,15 @@ var sheepCounted = 0;
  sheepCounted++;
 }
 console.log("Zzzzzzzzzzz");
+I have counted 0 sheep!
+I have counted 1 sheep!
+I have counted 2 sheep!
+I have counted 3 sheep!
+I have counted 4 sheep!
+I have counted 5 sheep!
+I have counted 6 sheep!
+I have counted 7 sheep!
+I have counted 8 sheep!
+I have counted 9 sheep!
+Zzzzzzzzzzz
+
