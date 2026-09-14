f6e23103e4c76cbc4654af14ffe00a33ae0bacd3
Nsika Nxumalo <nxumalonsika83@gmail.com>
2026-09-14 08:32:35 +0200

    Update Page 100.js

--- Chap06/Page 100.js
+++ Chap06/Page 100.js
@@ -2,3 +2,7 @@ var animals = ["Lion", "Flamingo", "Polar Bear", "Boa Constrictor"];
 for (var i = 0; i < animals.length; i++) {
  console.log("This zoo contains a " + animals[i] + ".");
 };
+This zoo contains a Lion.
+This zoo contains a Flamingo.
+This zoo contains a Polar Bear.
+This zoo contains a Boa Constrictor.
